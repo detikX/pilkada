93c34eab63b892cd51f38c9564d3ede978707c37
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-12-09 16:20:32 +0700

    ffrfrf

--- js/data-dki.js
+++ js/data-dki.js
@@ -93,135 +93,49 @@ setTimeout(() => {
     })
     Highcharts.chart('container-dki', {
         chart: {
-            type: 'bar',
-            backgroundColor: 'rgba(255,255,255,.6)',
-            borderRadius: 16,
+            type: 'column'
         },
         title: {
-            text: null,
-            align: 'left',
-            style: {
-                // color: "#fafafa",
-                font: 'bold 2rem "Zain", sans-serif',
-
-                lineHeight: '4rem'
-            }
+            text: 'Corn vs wheat estimated production for 2023'
         },
         subtitle: {
-            text: 'Sumber: BPS',
-            align: 'left',
-            style: {
-                color: "#4a4a4a",
-                font: 'normal 13px "Source Code Pro", sans-serif',
-                // lineHeight: '2rem'
-            }
+            text:
+                'Source: <a target="_blank" ' +
+                'href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>'
         },
         xAxis: {
-            categories: ['15-19 ', '20-24 ', '25-29 ', '30-34 ', '35-39 ', '40-44 ', '45-49 ', '50-54 ', '55-59 ', '> 60 '],
-            title: {
-                text: 'Tahun'
-            },
-
-            labels: {
-                // text: null,
-                style: {
-                    // color: "#fafafa",
-                    font: '18px "Zain", sans-serif',
-                    // lineHeight: '2rem'
-                    textOutline: false
-                }
-            },
-            gridLineWidth: .7,
-            lineWidth: 0
+            categories: ['USA', 'China', 'Brazil', 'EU', 'Argentina', 'India'],
+            crosshair: true,
+            accessibility: {
+                description: 'Countries'
+            }
         },
         yAxis: {
             min: 0,
-            // title: {
-            //     text: 'Population (millions)',
-            //     align: 'high'
-            // },
-            labels: {
-                overflow: 'justify',
-                enabled: false
-            },
             title: {
-                text: null
-            },
-            gridLineWidth: 0
+                text: '1000 metric tons (MT)'
+            }
         },
         tooltip: {
-            // formatter: function () {
-            //     var a = this.y.toString().replace('.', ',');
-            //     return this.series.name + ': <b>' + a + '%</b>';
-
-            // },
-            // valueSuffix: '%',
-            // value: '{point.name} tahun'
-            formatter: function () {
-                var a = this.y.toString().replace('.', ',');
-                return 'Usia: ' + this.x + ' tahun<br>Pengangguran di tahun <b>' + this.series.name +
-                    '</b><br> mencapai <b>' + a + '%</b>';
-            },
-            style: {
-                // fontWeight: 'bold',
-                fontFamily: 'Zain',
-                fontSize: '1.2rem'
-            }
+            valueSuffix: ' (1000 MT)'
         },
         plotOptions: {
-            // series: {
-            //     stacking: 'normal',
-            // },
-            bar: {
-                borderRadius: '50%',
-                borderColor: 'transparent',
-                dataLabels: {
-                    enabled: true,
-                    formatter: function () {
-                        var a = this.y.toString().replace('.', ',');
-                        // var pcnt = (data[this.x].y / dataSum) * 100;
-                        return '<span>' + a + ' %' + '</span>';
-                    },
-                    style: {
-                        // color: "#fafafa",
-                        font: 'normal 1.5rem "Zain", sans-serif',
-                        // lineHeight: '2rem'
-                        fontWeight: 'normal',
-                        textOutline: false
-                    },
-                },
-                groupPadding: 0.1
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
             }
         },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'bottom',
-            x: -10,
-            y: 10,
-            floating: true,
-            borderWidth: 1,
-            itemStyle: {
-                font: 'normal .9rem Zain',
-                // color: '#A0A0A0'
+        series: [
+            {
+                name: 'Corn',
+                data: [387749, 280000, 129000, 64300, 54000, 34300]
             },
-            backgroundColor:
-                Highcharts.defaultOptions.legend.backgroundColor || 'rgba(255,255,255,.7)',
-            shadow: false
-        },
-        credits: {
-            enabled: false //buat highcharts com
-        },
-        series: [{
-            name: '2021',
-            data: [23.91, 17.73, 9.26, 5.43, 4.02, 3.42, 3.30, 2.18, 1.98, 2.73]
-        }, {
-            name: '2022',
-            data: [29.08, 17.02, 7.13, 3.70, 2.65, 2.43, 2.33, 2.38, 2.37, 2.85]
-        }, {
-            name: '2023',
-            data: [25.77, 16.85, 7.48, 3.55, 2.54, 1.82, 1.80, 1.79, 1.52, 1.28]
-        }]
+            {
+                name: 'Wheat',
+                data: [45321, 140000, 10000, 140500, 19500, 113500]
+            }
+        ]
     });
+
 
 }, 1000);
